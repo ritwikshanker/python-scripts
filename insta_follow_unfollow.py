3120af7c9b869fd8e59751684fe759b686deea3b
ritwikshanker <ritwikshanker@yahoo.in>
2024-02-05 00:23:37 +0100

    Added a few more stats

--- insta_follow_unfollow.py
+++ insta_follow_unfollow.py
@@ -18,7 +18,8 @@ def get_users(json_data):
 
 def check_following_back(followers, following):
     not_following_back = [user for user in following if user not in followers]
-    return not_following_back
+    me_not_following_back = [user for user in followers if user not in following]
+    return not_following_back, me_not_following_back
 
 
 if __name__ == "__main__":
@@ -26,10 +27,15 @@ if __name__ == "__main__":
     following_json = load_json("following.json")["relationships_following"]
 
     followers = get_users(followers_json)
+    print("Total Number of Followers : " + str(len(followers)))
     following = get_users(following_json)
-
-    not_following_back = check_following_back(followers, following)
-
+    print("Total Number of Following : " + str(len(following)))
+    not_following_back, me_not_following_back = check_following_back(followers, following)
+    print("Total Number of People not Following back : " + str(len(not_following_back)))
     print("Users not following you back:")
     for user in not_following_back:
+        print(user)
+    print("\nTotal Number of People I'm not following back : " + str(len(me_not_following_back)))
+    print("Users whom you are not following back:")
+    for user in me_not_following_back:
         print(user)
